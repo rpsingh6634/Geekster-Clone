a5ea8255c795916bdacbdee6ea63c5703ec9e7bd
rpsingh6634 <rpsingh6634@gmail.com>
2022-09-14 13:38:48 +0530

    first commit

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,150 @@
+import { useState, useEffect } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import { sliderData } from "../../slider-data";
+import "./Slider.css";
+import youtube from './logo/youtube.svg';
+
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
+  const CurrentSlide = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Saurabh-Garg.webp" ? 0 : "https://images.geekster.in/new-testimonial/Saurabh-Garg.webp");
+  }
+  const CurrentSlide1 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Naveen-Katiyar.webp" ? 1 : "https://images.geekster.in/new-testimonial/Naveen-Katiyar.webp");
+  }
+  const CurrentSlide2 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Kaushal-Sorathiya.webp" ? 2 : "https://images.geekster.in/new-testimonial/Kaushal-Sorathiya.webp");
+  }
+  const CurrentSlide3 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Sai-Pranav-Tunuguntla.webp" ? 3 : "https://images.geekster.in/new-testimonial/Sai-Pranav-Tunuguntla.webp");
+  }
+  const CurrentSlide4 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Gajendra-Malviya.webp" ? 4 : "https://images.geekster.in/new-testimonial/Gajendra-Malviya.webp");
+  }
+  const CurrentSlide5 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Nipun-Sinha.webp" ? 5 : "https://images.geekster.in/new-testimonial/Nipun-Sinha.webp");
+  }
+  const CurrentSlide6 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Utkarsh-singh.webp" ? 6 : "https://images.geekster.in/new-testimonial/Utkarsh-singh.webp");
+  }
+  const CurrentSlide7 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Prakhar-Agarwal.webp" ? 7 : "https://images.geekster.in/new-testimonial/Prakhar-Agarwal.webp");
+  }
+  const CurrentSlide8 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Karri-Devi.webp" ? 8 : "https://images.geekster.in/new-testimonial/Karri-Devi.webp");
+  }
+  const CurrentSlide9 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/new-testimonial/Pawan-Kumar.webp" ? 9 : "https://images.geekster.in/new-testimonial/Pawan-Kumar.webp");
+  }
+  const CurrentSlide10 = () => {
+    setCurrentSlide(sliderData !== "https://images.geekster.in/placed/Vamsi-Krishna.webp" ? 10 : "https://images.geekster.in/placed/Vamsi-Krishna.webp");
+  }
+
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+    console.log("next");
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+    console.log("prev");
+  };
+
+  function auto() {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoScroll) {
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentSlide]);
+  return (
+    <div>
+      <center><h1 className="hone"><span className="span">Thousands</span> of lives changed</h1></center>
+      <center><h4 className="hfour">Hear it from the ones who have been on this journey already</h4></center>
+      <div className="row">
+        <div className="column1">
+          <div className="one" >
+            <img className="img1" src={"https://images.geekster.in/new-testimonial/Saurabh-Garg.webp"} alt="logo" onClick={() => { CurrentSlide(); }} />
+            <img className="img2" src={"https://images.geekster.in/new-testimonial/Naveen-Katiyar.webp"} alt="logo" onClick={() => { CurrentSlide1(); }} />
+          </div>
+          <div className="two">
+            <img className="img3" src={"https://images.geekster.in/new-testimonial/Kaushal-Sorathiya.webp"} alt="logo" onClick={() => { CurrentSlide2(); }} />
+          </div>
+          <div className="three">
+            <img className="img4" src={"https://images.geekster.in/new-testimonial/Sai-Pranav-Tunuguntla.webp"} alt="logo" onClick={() => { CurrentSlide3(); }} />
+            <img className="img5" src={"https://images.geekster.in/new-testimonial/Gajendra-Malviya.webp"} alt="logo" onClick={() => { CurrentSlide4(); }} />
+          </div>
+          <div className="seven">
+            <img className="img6" src={"https://images.geekster.in/new-testimonial/Nipun-Sinha.webp"} alt="logo" onClick={() => { CurrentSlide5(); }} />
+          </div>
+        </div>
+
+        <div className="column2">
+          <AiOutlineLeft className="arrow prev" onClick={prevSlide} />
+          <AiOutlineRight className="arrow next" onClick={nextSlide} />
+          <div className="slider" id="sliderdata">
+            {sliderData.map((slide, index) => {
+              return (
+                <div>
+                  <div className="row1">
+                    <div
+                      className={index === currentSlide ? "slide current" : "slide"}
+                      key={index}>
+                      <div className="column3">
+                        {index === currentSlide && (
+                          <div>
+                            <img src={slide.image} alt="slide" className="image" />
+                            <h4 className="name">{slide.title}</h4>
+                            <img src={slide.subTitle} alt="slide" id="company" />
+                          </div>
+                        )}</div>
+                      <div className="column4">
+                        <img className="youtube" src={youtube} alt="logo" />
+                        <span>
+                        <a className="visit" href="https://www.youtube.com/watch?v=D_Tz9LOzX6Q&feature=emb_imp_woyt" >Watch Testimonial</a>
+                        </span>
+                        <p className="description" >{slide.desc}</p>
+                        <br />
+                        <a className="readmore" href="https://geekster.in/student-testimony?name=Kaushal+Sorathiya">Read More</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <button className="more">Read More Testimonials</button>
+        </div>
+        <div className="column5">
+          <div className="four">
+            <img className="img7 " src={"https://images.geekster.in/new-testimonial/Utkarsh-singh.webp"} alt="logo" onClick={() => { CurrentSlide6(); }} />
+            <img className="img8" src={"https://images.geekster.in/new-testimonial/Prakhar-Agarwal.webp"} alt="logo" onClick={() => { CurrentSlide7(); }} />
+          </div>
+          <div className="five">
+            <img className="img9" src={"https://images.geekster.in/new-testimonial/Karri-Devi.webp"} alt="logo" onClick={() => { CurrentSlide8(); }} />
+          </div>
+          <div className="six">
+            <img className="img10" src={"https://images.geekster.in/new-testimonial/Pawan-Kumar.webp"} alt="logo" onClick={() => { CurrentSlide9(); }} />
+            <img className="img11" src={"https://images.geekster.in/placed/Vamsi-Krishna.webp"} alt="logo" onClick={() => { CurrentSlide10(); }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Slider;
---
+++ src/slider-data.js
@@ -0,0 +1,100 @@
+export const sliderData =[
+    {
+      id: 1,
+      image: "https://images.geekster.in/new-testimonial/Saurabh-Garg.webp",
+      title: "Shaurabh Garg",
+      subTitle: "https://images.geekster.in/hiring-partners/publicis-sapient.webp",
+      target:"https://geekster.in/student-testimony",
+      link: "https://www.youtube.com/watch?v=D_Tz9LOzX6Q&feature=emb_imp_woyt",
+      desc:"Geeksters supportive team taught me advanced Java concepts, helped me with interview preparation, and provided various reading materials. Besides this, they brought many..."
+    },
+    {
+      id: 2,
+      image: "https://images.geekster.in/new-testimonial/Naveen-Katiyar.webp",
+      title: "Naveen Katiyar",
+      subTitle: "https://images.geekster.in/hiring-partners/rakuten.webp",
+      link: "vAoYyYtSrvs",
+      target:"https://geekster.in/student-testimony",
+      desc:"Geekster is an excellent platform for learning new software development technologies to crack interviews at reputed companies. Mentors solve doubts in every possible way..."
+    },
+    {
+      id: 3,
+      image: "https://images.geekster.in/new-testimonial/Kaushal-Sorathiya.webp",
+      title: "Kaushal Sorathiya",
+    subTitle: "https://images.geekster.in/hiring-partners/quinta.webp",
+    link: "_hWP0gK3_iA",
+    target:"https://geekster.in/student-testimony",
+    desc:"Being a newbie in the coding world i was underconfident about my skills earlier but with Geekster one to one mentoring and guidance, I am currently working as a full stack java..."
+    },
+    {
+      id: 4,
+        image: "https://images.geekster.in/new-testimonial/Sai-Pranav-Tunuguntla.webp",
+        title: "Sai Pranav Tunuguntla",
+        subTitle: "https://images.geekster.in/hiring-partners/smart-docs.webp",
+        link: "qNBUVp6aQRk",
+        target:"https://geekster.in/student-testimony",
+        desc:"I got all the help and mentorship I needed to ace my full stack interviews. Before joining Geekster I did not know how to go about navigating the job world as a..."
+      },
+      {
+        id: 5,
+        image: "https://images.geekster.in/new-testimonial/Gajendra-Malviya.webp",
+        title: "Gajendra Malviya",
+        subTitle: "https://images.geekster.in/hiring-partners/riskcovry.webp",
+        link: "YOWCnOrQYIU",
+        target:"https://geekster.in/student-testimony",
+        desc:"From '0 offers' to 'multiple offers, that has been my journey at Geekster. My happiness multiplies when I see my proud family. It is the learnings and guidance I..."
+      },
+      {
+        id: 6,
+        image: "https://images.geekster.in/new-testimonial/Nipun-Sinha.webp",
+        title: "Nipun Sinha",
+        subTitle: "https://images.geekster.in/hiring-partners/societe-generale.webp",
+        link: "e3YqJkAWpgI",
+        target:"https://geekster.in/student-testimony",
+        desc:"I worked on my basic to advanced technical skills under Geekster's expert mentors. I realised that other group students also had zero technical knowledge, but as the..."
+      },
+      {
+        id: 7,
+        image: "https://images.geekster.in/new-testimonial/Utkarsh-singh.webp",
+        title: "Utkarsh Singh",
+        subTitle: "https://images.geekster.in/hiring-partners/Bluewud.webp",
+        link:"Hcz8rFcaEa4",
+        target:"https://geekster.in/student-testimony",
+        desc:"Geekster taught me in months what years of college work couldn't teach me. They honed my technical and non-technical skills, build my confidence and gave me a flavor of..."
+      },
+      {
+        id: 8,
+        image: "https://images.geekster.in/new-testimonial/Prakhar-Agarwal.webp",
+        title: "Prakhar Agarwal",
+        subTitle: "https://images.geekster.in/hiring-partners/PayPal.webp",
+        target:"https://geekster.in/student-testimony",
+        desc:"What a wonderful journey I had at Geekster! To be honest they exceeded my expectations. I would have never imagined a 294% hike in the beginning of my career. But Geekster made it..."
+      },
+      {
+        id: 9,
+        image: "https://images.geekster.in/new-testimonial/Karri-Devi.webp",
+        title: "Karri Devi",
+        subTitle: "https://images.geekster.in/hiring-partners/publicis-sapient.webp",
+        link: "Ksxy1BZb5bI",
+        target:"https://geekster.in/student-testimony",
+        desc:"I did not know enough about coding before joining Geekster and had no job. All my friends had jobs by then. I joined Geekster and learned everything about full stack..."
+      },
+      {
+        id: 10,
+        image: "https://images.geekster.in/new-testimonial/Pawan-Kumar.webp",
+        title:"Pawan Kumar",
+        subTitle: "https://images.geekster.in/hiring-partners/Radiansys.webp",
+        link: "hVFOXor5wgs",
+        target:"https://geekster.in/student-testimony",
+        desc:"Geekster educators were very good. We used to make live projects and with the help of educators and mentors I was able to transition from Petroleum engineering to software..."
+      },
+      {
+        id: 11,
+        image: "https://images.geekster.in/placed/Vamsi-Krishna.webp",
+        title: "Vamsi Krishna",
+        subTitle: "https://images.geekster.in/hiring-partners/avizva.webp",
+        target:"https://geekster.in/student-testimony",
+        desc:"I have had a great learning experience with Geekster. They not just focus on technical skills but prepare us for interviews through soft-skills training as well. The..."
+      },
+    ];
+  
